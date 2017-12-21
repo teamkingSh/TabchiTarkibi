98f81165ccc0fba74dc82bd94a3b18f906c0002a
TeamKingSH <33286921+teamkingSh@users.noreply.github.com>
2017-12-21 12:24:43 +0330

    Update tabchi.lua

--- bot/tabchi.lua
+++ bot/tabchi.lua
@@ -14,7 +14,7 @@ https = require "ssl.https"
 -----~~~~~~~~~~~~~~~~
 d = dofile('./libs/redis.lua')
 -------------------------------
-config_sudo = {987654321}
+config_sudo = {331807898}
 function dl_cb(arg, data)
 end
 function is_sudo(msg)
